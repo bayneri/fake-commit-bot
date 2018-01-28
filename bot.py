7a9c7659afed8bc087acdcac4a35a8b33a4c87c6
Halil Çetiner <hhalilcetiner@gmail.com>
2018-01-28 15:32:19 +0300

    minor fix

--- bot.py
+++ bot.py
@@ -2,10 +2,8 @@ import subprocess
 import random
 
 rand = random.randint(0,10)
-print('----------------')
-print(rand)
 
-if rand > 5:
+if rand >= 5:
     # my little tiny miny fake github activity
     subprocess.Popen(["bash", "/Users/halil/Desktop/funfunfun/fake-commit-bot/git.sh"])
 else:
