e298c0d0ee85e98c65c9a2b10824f69e90432704
Halil Çetiner <hhalilcetiner@gmail.com>
2018-01-28 15:30:38 +0300

    Commit: Update commits @ Sun Jan 28 15:30:38 +03 2018

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import subprocess
 import random
 
 rand = random.randint(0,10)
+print('----------------')
+print(rand)
 
 if rand > 5:
     # my little tiny miny fake github activity
-    subprocess.Popen(["bash", "./git.sh"])
+    subprocess.Popen(["bash", "/Users/halil/Desktop/funfunfun/fake-commit-bot/git.sh"])
+else:
+    print('unlucky')
--- commits.md
+++ commits.md
@@ -1,1 +1,2 @@
 Commit: Update commits @ Sun Jan 28 14:39:52 +03 2018
+Commit: Update commits @ Sun Jan 28 15:30:38 +03 2018
